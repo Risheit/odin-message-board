f3f11bc8db7f213433104c2e085b69a772c19fea
Risheit <80658104+Risheit@users.noreply.github.com>
2023-01-02 17:32:10 -0600

    Create message board index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+class Message {
+  constructor(text, user, added) {
+    this.text = text;
+    this.user = user;
+    this.added = added;
+  }
+}
+
+const messages = [
+   new Message("Hi There!", "Amando", new Date()),
+   new Message("Hello World!", "Charles", new Date())
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { messages: messages });
 });
 
 module.exports = router;
